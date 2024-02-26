4a43f15a6ae6a5650fd97892f8407626b823cf3f
jiohh <dkswlgh99@naver.com>
2024-02-27 01:54:05 +0900

    fix : 2579 계단 오르기 풀이 수정

--- baekjoon/2579.py
+++ baekjoon/2579.py
@@ -4,12 +4,17 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [0,0,int(input()),0]
+stairs = [0] * 300 # n이 아닌 300을 곱해주는 이유 : 아래에서 indexError가 발생하기 때문
+dp = [0] * 300
 
-for i in range(n-2):
-    temp = int(input())
-    print(dp)
-    dp[0], dp[1],dp[2] = max(dp), dp[0] + temp, dp[1] + temp
+for i in range(n) :
+    stairs[i] = int(input()) #append를 사용하지 않는 이유 밑에 초기화 때문에 indexError가 발생한다.
 
-print(dp)
-print(max(dp[:-1])+int(input()))
+dp[0] = stairs[0]
+dp[1] = stairs[1] + stairs[0]
+dp[2] = max(stairs[0]+stairs[2], stairs[1]+stairs[2])
+
+for i in range(3,n) :
+    dp[i] = max(dp[i-3]+stairs[i-1]+stairs[i], dp[i-2]+stairs[i])
+
+print(dp[n-1])
