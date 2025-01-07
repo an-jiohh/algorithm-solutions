a0c734c1d68a44a70b1bf6906d79a74cb08cb615
an-jiohh <dkswlgh99@naver.com>
2025-01-07 14:04:58 +0900

    feat : 11052 카드 구매하기

--- baekjoon/11052.py
+++ baekjoon/11052.py
@@ -1,4 +1,12 @@
 n = int(input())
 
 arr = list(map(int, input().split()))
+dp = [0 for i in range(n+1)]
 
+arr.insert(0,0)
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i-j] + arr[j], dp[i])
+
+print(dp[n])
