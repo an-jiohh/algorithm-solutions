09dcbfb1e990816b8a5957a85d31f89e1bf39a81
an-jiohh <dkswlgh99@naver.com>
2025-01-09 23:40:56 +0900

    feat : 2178 미로탐색 풀이

--- baekjoon/2178.py
+++ baekjoon/2178.py
@@ -1,7 +1,20 @@
 n, m = map(int, input().split())
 
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [input() for _ in range(n)]
+from collections import deque
 
-print(n)
-print(m)
-print(arr)
+queue = deque([(0,0,1)])
+
+direction = [(0,1),(1,0),(-1,0),(0,-1)]
+visited = [[0]*m for _ in range(n)]
+
+while queue :
+    y,x,count = queue.popleft()
+    if y==n-1 and x == m-1 :
+        print(count)
+        break
+    for move_y, move_x in direction :
+        if 0 <= x+move_x < m and 0<=y+move_y < n :
+            if arr[y+move_y][x+move_x]=="1" and visited[y+move_y][x+move_x] == 0:
+                visited[y+move_y][x+move_x] = 1
+                queue.append((y+move_y,x+move_x,count+1))
