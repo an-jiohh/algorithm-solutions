db59e0dd1bce4e59c009be168b7fa8eac40266a9
an-jiohh <dkswlgh99@naver.com>
2025-02-12 20:35:09 +0900

    faet : 21756 지우개 풀이

---
+++ baekjoon/21756.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+arr = [i for i in range(1, n+1)]
+
+while len(arr) != 1 :
+    temp = []
+    for i in range(0, len(arr)):
+        if (i + 1) % 2 == 0 :
+            temp.append(arr[i])
+    arr = temp
+print(*arr)
