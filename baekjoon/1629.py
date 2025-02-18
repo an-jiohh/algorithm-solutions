19036c3a4b06596b6094c6fe3744b0d369d31714
an-jiohh <dkswlgh99@naver.com>
2025-02-18 15:32:23 +0900

    feat : 1629 곱셈 개선 풀이

--- baekjoon/1629.py
+++ baekjoon/1629.py
@@ -1,3 +1,18 @@
+# a,b,c = map(int, input().split())
+
+# print(pow(a,b,c))
+
 a,b,c = map(int, input().split())
 
-print(pow(a,b,c))
+def dfs(a,b):
+    if b == 1 :
+        return a % c
+    
+    answer = dfs(a, b // 2)
+
+    if b % 2 == 0 :
+        return (answer * answer) % c
+    else :
+        return (answer * answer) * a % c
+    
+print(dfs(a,b))
