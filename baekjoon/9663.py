dd74999e9e2079a863f221c93c474efe0ca5762f
an-jiohh <dkswlgh99@naver.com>
2025-05-15 11:20:25 +0900

    feat : 9663 N-Queen 풀이

--- baekjoon/9663.py
+++ baekjoon/9663.py
@@ -1,3 +1,4 @@
+"""
 n = int(input())
 
 arr = [0 for i in range(15)]
@@ -21,3 +22,29 @@ def dfs(row) :
         arr[row] = i - 1
 dfs(0)
 print(count)
+"""
+n = int(input())
+
+arr = [0] * n #0으로 정해도되는 이유 -> index0은 어차피 dfs에서 처음으로 값이 채워짐
+answer = 0
+
+def check(row):
+    for i in range(row):
+        if arr[i] == arr[row] : #같은 열에 퀸이 존재하는 경우
+            return False
+        if row - i == abs(arr[row]-arr[i]) :
+            return False
+    return True
+
+def dfs(row):
+    if row == n :
+        return 1
+    answer = 0
+    for i in range(n):
+        arr[row] = i 
+        if check(row) :
+            answer += dfs(row + 1)
+    return answer
+
+print(dfs(0))
+        
