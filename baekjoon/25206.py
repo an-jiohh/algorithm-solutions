3baae3c3f41069f5fafb072f56ed86d1872aea79
an-jiohh <dkswlgh99@naver.com>
2026-04-11 10:15:02 +0900

    feat : 25206 너의 평점은 풀이

---
+++ baekjoon/25206.py
@@ -0,0 +1,23 @@
+score_board = {
+"A+":	4.5,
+"A0":	4.0,
+"B+":	3.5,
+"B0":	3.0,
+"C+":	2.5,
+"C0":	2.0,
+"D+":	1.5,
+"D0":	1.0,
+"F":0.0
+}
+
+total_score = 0
+total_credit = 0
+
+for i in range(20):
+    _, score, grade = input().split()
+    if grade == "P" :
+        continue
+    total_score += float(score) * score_board[grade]
+    total_credit += float(score)
+
+print(total_score / total_credit)
